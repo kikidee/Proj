af7838fa1d296094b45b46d18fc1436b5f4a29c4
unknown <nicolakirsten@gmail.com>
2013-04-15 18:05:05 +0100

    prettied up lisowski graph

--- vol_strain.m
+++ vol_strain.m
@@ -1,9 +1,11 @@
-% Function to calculate the radial surface strain 
+%--------------------------------------------------------------------------------
+% Function to calculate displacement and strain 
 % from a spherical source at depth
+%--------------------------------------------------------------------------------
 
-%--------------------------------------------------
-
-%variables:
+%--------------------------------------------------------------------------------
+% Variables:
+%--------------------------------------------------------------------------------
 
 % d = source depth [m]
 % a = source radius [m]
@@ -14,11 +16,11 @@
 % x = radial distance on the surface [m]
 % ur = radial displacement
 
-%----------------------------------------------------
-
 clear all
 close all
-%Inputs
+%----------------------------------------------------------------------------------
+% Inputs
+%----------------------------------------------------------------------------------
 
 d = 5000;
 a = 1000;    
@@ -26,12 +28,18 @@ P = 10e6;
 nu = 0.25;
 E = 20e9;
 G = 8e9;
-r = -25000:1:25000;
+r = -15000:5:15000;
 
-%calcuate ur from mogi equation
+%----------------------------------------------------------------------------------
+%Calcuate Displacement from Mogi equation
+%----------------------------------------------------------------------------------
+
 ur = P.*(a.^3).*(1-nu)./G.*(r./(r.^2+d.^2).^1.5);
 uz = P.*(a.^3).*(1-nu)./G.*(d./(r.^2+d.^2).^1.5);
-% Calculations
+
+%----------------------------------------------------------------------------------
+% Calculate Strain Components
+%----------------------------------------------------------------------------------
 
 % Err = radial displacement/radial distance
 Rstrain = diff(ur)./diff(r);
@@ -39,37 +47,30 @@ Rstrain = diff(ur)./diff(r);
 % E00 = radial displacement/radial distance
 Tstrain = ur./r;
 
+%Ezz = vertical strain
+Vert_strain = (nu/(1-nu))*(Rstrain+Tstrain(:,1:max(size(Tstrain))-1));
+Vert_strain =  Vert_strain*-1;
+
 % normalising factor
 %Normal = (3*P*(a^3))/(4*G*d^3);
 
 norm_r_strain = Rstrain/((3*P*a^3)/(4*G*d^3));
-
 norm_tang_strain = Tstrain/((3*P*a^3)/(4*G*d^3)); %ad
-
-%Ezz = vertical strain
-Vert_strain = (nu/(1-nu))*(Rstrain+Tstrain(:,1:max(size(Tstrain))-1));
-
-%Normalised Ezz
 norm_vert_strain = ((nu/(1-nu))*(Rstrain+Tstrain(:,1:max(size(Tstrain))-1)))/((3*P*a^3)/(4*G*d^3));
-Vert_strain =  Vert_strain*-1;
 norm_vert_strain = norm_vert_strain*-1;
 
+%----------------------------------------------------------------------------------
+% Calculate Volumetric Strain
+%----------------------------------------------------------------------------------
 
 %Volumetric strain = Err + E00 + Ezz
-
 Vol_strain = (Vert_strain + Tstrain(:,1:max(size(Tstrain))-1) + Rstrain);
 norm_vol_strain= (Vert_strain + Tstrain(:,1:max(size(Tstrain))-1) + Rstrain)/((3*P*a^3)/(4*G*d^3));
-
 source_depth = r/d;
 
-%plot (r(:,1:max(size(r))-1), Vol_strain);
-
-%legend('Volumetric Strain')
-%xlabel('Distance (source depth)')
-%ylabel('Volumetric Strain')
-
-%------------------------------------
-%Call comsol benchmark data
+%----------------------------------------------------------------------------------
+% Call COMSOL Benchmark Data
+%----------------------------------------------------------------------------------
 
 spherray=load('sphere_bench.txt', '-ascii');
 dist=spherray(:,1);
@@ -82,30 +83,32 @@ vertstrain= (nu/(1-nu))*(tangstrain(1:100)+radstrain);
 vstrain = (tangstrain(1:100)+radstrain + vertstrain(1:100));
 
 %------------------------------------------------------------------------------
-%Plot Data
+% Plot Data
 %------------------------------------------------------------------------------
 
-figure(1)
-hold on
-plot(r(1:50000),Vol_strain,'r','LineWidth',2)
-for i=1:max(size(volstrain))
-  if (mod(i,4) == 0)
-    scatter(dist(i),volstrain(i),60,'g', 'fill')
-  end
-end
-plot(dist,volstrain, 'g', 'LineWidth',2)
+%figure(1)
+%hold on
+%plot(r(1:50000),Vol_strain,'r','LineWidth',2)
+%for i=1:max(size(volstrain))
+%  if (mod(i,4) == 0)
+%    scatter(dist(i),volstrain(i),60,'g', 'fill')
+%  end
+%end
+plot(source_depth(1:6000),norm_vol_strain, 'LineWidth',2)
 
+%----------------------------------------------------------------------------------
+% Tidy Graph
+%----------------------------------------------------------------------------------
 
 % Set Graph Title in fontsize
-title('Sphere Benchmark at 5km depth', 'FontSize', 12, 'FontName', 'Arial');
+title('Volumetric Strain (Lisowski)', 'FontSize', 12, 'FontName', 'Arial');
 
 % Set Axis
-xlabel('Distance (meters)', 'FontSize', 12, 'FontName', 'Arial')
-ylabel('Strain (strain units)', 'FontSize', 12)
+xlabel('Distance (Top Depths)', 'FontSize', 12, 'FontName', 'Arial')
+ylabel('Volumetric Strain (strain units)', 'FontSize', 12)
 
 % Set Graph Background Color
 set(gca,'Color',[1 1 1]);
-
 
 % Show Grid Lines
 %grid minor
@@ -114,18 +117,24 @@ grid off
 
 % Set Graph Limits
 %xMin xMax yMin yMax
-axis([-1.e4 1.e4 -2e-7 10.5e-6])
+%axis([-1.e4 1.e4 -2e-7 10.5e-6])
 waitforbuttonpress()
 
 %legend command
-line_1_name = 'Analytical Model';
-line_2_name = 'Numerical Model';
-legend(line_1_name, line_2_name, 'Location','NorthEast')
+%line_1_name = 'Analytical Model';
+%line_2_name = 'Numerical Model';
+%legend(line_1_name, line_2_name, 'Location','NorthEast')
+
+%----------------------------------------------------------------------------------
+% Save high resolution version of graph to working directory
+%----------------------------------------------------------------------------------
 
 % Save figure 1 to jpeg,called output_test.jpg
 % at a resolution of 500 dots per inch
 % text is (for commercial print) 300
 % images are 2000
-print(1,'-djpeg','output_sphere_5','-r500')
+print(1,'-djpeg','lisowski_sphere_volumetric','-r500')
+
+%----------------------------------------------------------------------------------
 
 
